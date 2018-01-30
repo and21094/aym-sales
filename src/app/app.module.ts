1396a79a5f87be7a7daaf1f26e887316c8b6ce03
marck0793 <marck0793@hotmail.com>
2018-01-29 18:34:33 -0600

    open and close cashBox buttons in toolbar and modalComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, 
-  MatMenuModule, MatToolbarModule, MatIconModule, MatTableModule } from '@angular/material';
+  MatMenuModule, MatToolbarModule, MatIconModule, MatTableModule, MatDialogModule } from '@angular/material';
 import {routing} from './app.routing';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { LoginComponent } from './login/login.component';
 import { BillsComponent } from './bills/bills.component';
 import { CashBoxComponent } from './cash-box/cash-box.component';
+import { ModalComponent } from './modal/modal.component';
 
 
 
@@ -18,7 +19,8 @@ import { CashBoxComponent } from './cash-box/cash-box.component';
     MainMenuComponent,
     LoginComponent,
     BillsComponent,
-    CashBoxComponent
+    CashBoxComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
@@ -31,9 +33,13 @@ import { CashBoxComponent } from './cash-box/cash-box.component';
     MatFormFieldModule,
     MatInputModule,
     MatTableModule,
+    MatDialogModule,
     routing
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    ModalComponent
+  ],
 })
 export class AppModule { }
